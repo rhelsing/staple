ecc0c67b5b80de883cb9268ef623c74f421d708e
Ryan Helsing <ryanhelsing@gmail.com>
2014-11-10 12:44:51 -0500

    better install

--- lib/staple/install_generator.rb
+++ lib/staple/install_generator.rb
@@ -11,9 +11,32 @@ module Staple
       generate "simple_form:install", "--foundation" #rails generate simple_form:install --foundation
     end
 
-    #modify simple form to use hints as errors
+    def remove_tree
+      gsub_file "app/assets/stylesheets/application#{detect_css_format[0]}", "\n#{detect_css_format[1]} require_tree .\n", ""
+    end
+
+    def include_font_awesome
+      insert_into_file "app/assets/stylesheets/application#{detect_css_format[0]}", "\n#{detect_css_format[1]} require font-awesome\n", :after => "require foundation_and_overrides"
+    end
+
+    def simplify_foundation_and_overrides
+      copy_file "source/foundation_and_overrides.scss" "app/assets/stylesheets/foundation_and_overrides.scss", :force => true
+    end
+
+    def modify_simple_form
+      gsub_file "config/initializers/simple_form_foundation.rb", "b.use :error, wrap_with: { tag: :small }", "b.use :error, :wrap_with => { :tag => :small, :class => :error }"
+      gsub_file "config/initializers/simple_form_foundation.rb", "# b.use :hint,  wrap_with: { tag: :span, class: :hint }", "b.use :hint,  wrap_with: { tag: :small, class: :error }"
+    end
 
     #import base style
+
+    def detect_css_format
+      return ['.css', ' *='] if File.exist?('app/assets/stylesheets/application.css')
+      return ['.css.sass', ' //='] if File.exist?('app/assets/stylesheets/application.css.sass')
+      return ['.sass', ' //='] if File.exist?('app/assets/stylesheets/application.sass')
+      return ['.css.scss', ' //='] if File.exist?('app/assets/stylesheets/application.css.scss')
+      return ['.scss', ' //='] if File.exist?('app/assets/stylesheets/application.scss')
+    end
 
   end
 end
