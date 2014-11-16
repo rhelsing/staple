4d7b9f0f8cf21135ee1703e0b262c58a03057a57
Ryan Helsing <ryanhelsing@gmail.com>
2014-11-16 12:51:46 -0500

    linebreak regex fixes

--- lib/staple.rb
+++ lib/staple.rb
@@ -1,4 +1,5 @@
 require 'staple/import_generator'
 require 'staple/append_generator'
 require 'staple/remove_generator'
+require 'staple/amputate_generator'
 require 'staple/install_generator'
--- lib/staple/remove_generator.rb
+++ lib/staple/remove_generator.rb
@@ -10,8 +10,8 @@ module Staple
         puts "remove from default"
         if contents
             #replace component
-            gsub_file "app/assets/stylesheets/staple/#{component}.scss", "\n#{contents}\n", ""
-            gsub_file "app/assets/stylesheets/staple/builders/build_#{component}.scss", "\n#{contents}\n", ""
+            gsub_file "app/assets/stylesheets/staple/#{component}.scss", "\n#{contents}", ""
+            gsub_file "app/assets/stylesheets/staple/builders/build_#{component}.scss", "\n#{contents}", ""
         else
             puts "invalid operation"
         end
