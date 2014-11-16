d47d02efeac3dbde6ff689f38a16305fb5d7f33b
Ryan Helsing <ryanhelsing@gmail.com>
2014-11-16 16:57:00 -0500

    hover append in foreach

--- lib/staple/append_generator.rb
+++ lib/staple/append_generator.rb
@@ -7,29 +7,30 @@ module Staple
     argument :actions, :type => :array, :default => []
 
     def append_styles
-        puts "append"
-        if contents
-            #replace component
-            gsub_file "app/assets/stylesheets/staple/#{component}.scss", "//&*append", "\n.#{component}.#{pattern}{\n\t#{contents}\n}//&*append"
-            # gsub_file "app/assets/stylesheets/staple/builders/build_#{component}.scss", "\n#{contents}//&*staple\n", "//&*staple"
-        else
-            puts "invalid operation"
-        end
+        gsub_file "app/assets/stylesheets/staple/#{component}.scss", "//&*append", "\n.#{component}.#{pattern}{\n\t#{contents}\n}//&*append" if contents
+        gsub_file "app/assets/stylesheets/staple/builders/build_#{component}.scss", "//&*append", "\n.#{component}.\#{class}.#{pattern}{\n\t#{contents}\n}//&*append" if contents
+        gsub_file "app/assets/stylesheets/staple/#{component}.scss", "//&*append", "\n.#{component}.#{pattern}:hover{\n\t#{contents}\n}//&*append" if hover
+        gsub_file "app/assets/stylesheets/staple/builders/build_#{component}.scss", "//&*append", "\n.#{component}.\#{class}.#{pattern}:hover{\n\t#{contents}\n}//&*append" if hover
     end
 
     private
 
     def contents
-        file = File.join(self.class.source_root, 'source', 'styles', "#{component}", pattern_name)
+        file = File.join(self.class.source_root, 'source', 'styles', "#{component}", "#{pattern.dasherize}.scss")
+        get_file(file)
+    end
+
+    def hover
+        file = File.join(self.class.source_root, 'source', 'styles', "#{component}", "#{pattern.dasherize}-hover.scss")
+        get_file(file)
+    end
+
+    def get_file(file)
         if File.file?(file)
             return File.read(file)
         else
             return false
         end
-    end
-
-    def pattern_name
-      "#{pattern.dasherize}.scss"
     end
 
     def component
