906d4a74ecb3f7416811de8c0e85ff530a6105ac
Ryan Helsing <ryanhelsing@gmail.com>
2014-11-20 18:22:22 -0500

    reset generator

---
+++ lib/staple/reset_generator.rb
@@ -0,0 +1,18 @@
+require 'rails/generators'
+
+module Staple
+  class ResetGenerator < Rails::Generators::Base
+    desc 'bring in the staple'
+    source_root File.join(File.dirname(__FILE__), '..', '..')
+    argument :actions, :type => :array, :default => []
+
+    def reset
+        components = %w(buttons colors forms tables typography sizes)
+        components.each do |c|
+            copy_file "source/styles/staple/#{c}.scss", "app/assets/stylesheets/staple/#{c}.scss", :force => true
+            copy_file "source/styles/staple/builders/build_#{c}.scss", "app/assets/stylesheets/staple/builders/build_#{c}.scss", :force => true
+        end
+    end
+
+  end
+end
