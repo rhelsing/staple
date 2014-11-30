9cd9edb6e8dba485c7c62bbe67e0eefafc26d380
Ryan Helsing <ryanhelsing@gmail.com>
2014-11-30 15:50:44 -0500

    confirm reset | include images

--- lib/staple/reset_generator.rb
+++ lib/staple/reset_generator.rb
@@ -7,10 +7,12 @@ module Staple
     argument :actions, :type => :array, :default => []
 
     def reset
-        components = %w(buttons colors forms tables typography sizes)
-        components.each do |c|
-            copy_file "source/styles/staple/#{c}.scss", "app/assets/stylesheets/staple/#{c}.scss", :force => true
-            copy_file "source/styles/staple/builders/build_#{c}.scss", "app/assets/stylesheets/staple/builders/build_#{c}.scss", :force => true
+        components = %w(buttons colors forms tables typography sizes images)
+        if yes? "All css changes will be overwritten. Continue? (Y/N)"
+            components.each do |c|
+                copy_file "source/styles/staple/#{c}.scss", "app/assets/stylesheets/staple/#{c}.scss", :force => true
+                copy_file "source/styles/staple/builders/build_#{c}.scss", "app/assets/stylesheets/staple/builders/build_#{c}.scss", :force => true
+            end
         end
     end
 
